266a2be7b758afe8c9ac35974b64baf1ec590f9e
Yaseen Mallick <62826473+YaseenMallick25@users.noreply.github.com>
2022-02-22 23:25:30 +0530

    Simple Rhino Plugin implemented Event Handling
    Added event handling with serial event handling used for rotating ActiveDoc => ActiveView to appropriate serial input reading

---
+++ My3DMouse/My3DMouseCommand.cs
@@ -0,0 +1,74 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+
+
+namespace My3DMouse
+{
+
+    [System.Runtime.InteropServices.Guid("31bb38dc-acf8-41e8-884d-81691693edb8")]
+    public class My3DMouseCommand : Command
+    {
+        public My3DMouseCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static My3DMouseCommand Instance
+        {
+            get; private set;
+        }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName
+        {
+            get { return "My3DMouseCommand"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            var enabled = My3DMouse.Instance.IsEnabled;
+            var prompt = enabled ? "Event watcher is enabled. New value" : "Event watcher is disabled. New value";
+
+            var go = new GetOption();
+            go.SetCommandPrompt(prompt);
+            go.AcceptNothing(true);
+
+            var d_option = go.AddOption("Disable");
+            var e_option = go.AddOption("Enable");
+            var t_option = go.AddOption("Toggle");
+
+            var res = go.Get();
+            if (res == GetResult.Nothing)
+                return Result.Success;
+            if (res != GetResult.Option)
+                return Result.Cancel;
+
+            var option = go.Option();
+            if (null == option)
+                return Result.Failure;
+
+            if (d_option == option.Index)
+            {
+                if (enabled)
+                    My3DMouse.Instance.Enable(false);
+            }
+            else if (e_option == option.Index)
+            {
+                if (!enabled)
+                    My3DMouse.Instance.Enable(true);
+            }
+            else if (t_option == option.Index)
+            {
+                My3DMouse.Instance.Enable(!enabled);
+            }
+
+            return Result.Success;
+        }
+    }
+}
---
+++ My3DMouse/My3DMousePlugIn.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO.Ports;
+using Rhino;
+using Rhino.Commands;
+using Rhino.PlugIns;
+
+namespace My3DMouse
+{
+    ///<summary>
+    /// <para>Every RhinoCommon .rhp assembly must have one and only one PlugIn-derived
+    /// class. DO NOT create instances of this class yourself. It is the
+    /// responsibility of Rhino to create an instance of this class.</para>
+    /// <para>To complete plug-in information, please also see all PlugInDescription
+    /// attributes in AssemblyInfo.cs (you might need to click "Project" ->
+    /// "Show All Files" to see it in the "Solution Explorer" window).</para>
+    ///</summary>
+    public class My3DMousePlugIn : Rhino.PlugIns.PlugIn
+
+    {
+        public My3DMousePlugIn()
+        {
+            Instance = this;
+        }
+
+        ///<summary>Gets the only instance of the My3DMousePlugIn plug-in.</summary>
+        public static My3DMousePlugIn Instance
+        {
+            get; private set;
+        }
+
+        public override PlugInLoadTime LoadTime => PlugInLoadTime.AtStartup;
+
+        /// <summary>
+        /// Called by Rhino when loading this plug-in.
+        /// </summary>
+        protected override LoadReturnCode OnLoad(ref string errorMessage)
+        {
+            RhinoApp.Initialized += RhinoApp_Initialized;
+            //My3DMouse.Instance.Enable(true);
+            return LoadReturnCode.Success;
+
+        }
+
+        private void RhinoApp_Initialized(object sender, EventArgs e)
+        {
+            My3DMouse.Instance.Enable(true);
+        
+        }
+
+        // You can override methods here to change the plug-in behavior on
+        // loading and shut down, add options pages to the Rhino _Option command
+        // and maintain plug-in wide options in a document.
+    }
+}
